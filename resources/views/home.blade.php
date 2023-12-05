014ca8a1ed4372bc82332991843da225f96f04f7
Héctor Ortiz <mail@hector.cat>
2023-12-05 12:06:17 +0100

    Min7 - 52

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center mt-5">
+                <h1 class="text-3xl font-bold underline">
+                    Hello world!
+                  </h1>
+                  <h2 class="text-red-400 text-4xl">La la  la la alaaaa</h2>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>InstaCirvi - @yield('title')</title>
+</head>
+<body class="bg-gray-100">
+    <header class="p-5 border-b bg-white shadow">
+      <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-3xl font-black">
+            InstaCirvi
+        </h1>
+
+        <nav class="flex gap-2">
+            <a class="font-bold uppercase text-gray-600" href="">Login</a>
+            <a class="font-bold uppercase text-gray-600 " href="">Register</a>
+        </nav>
+    </div>
+    </header>
+
+    <main class="container mx-auto mt-10">
+        <h2 class="font-black text-center text-3xl mb-10">@yield('title')</h2>
+        @yield('content')
+    </main>
+
+    <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        InstaCirvi - {{ now()->year }} &copy;
+        @php echo date('Y') @endphp
+
+    </footer>
+    
+    
+</body>
+</html>
